bf601ddb4182033ccdbd653371f44e2652fd2b6a
Doonoosoo <jose.ricardo.acosta@outlook.es>
2025-09-10 18:53:53 -0500

    Ejercicio 3 completado, averiguar como solocitar datos mediante consola en js

---
+++ reto_3.js
@@ -0,0 +1,5 @@
+//Escribe un programa que pida al usuario dos números y luego muestre en pantalla la suma de ambos.
+let num1 = 2
+let num2 = 8
+let suma = num1 + num2;
+console.log(`La suma es: ${suma}`);
---
+++ reto_3.py
@@ -0,0 +1,6 @@
+#Escribe un programa que pida al usuario dos números y luego muestre en pantalla la suma de ambos.
+x= input("Escribe un numero: ")
+y = input("Escribe otro numero: ")
+z= int(x) + int(y)
+
+print("La suma de esos numeros es:",z)
